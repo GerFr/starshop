ee7805c88b65903dfda6a92d961f16204ec540b1
GerFr <140012598+GerFr@users.noreply.github.com>
2024-04-09 23:22:57 +0200

    add js file for testing

---
+++ index.js
@@ -0,0 +1,13 @@
+// array
+// konstanter Arraytyp
+// um nichts
+let artikel = 0
+const artikelliste = ["Brokkoli"]
+console.log(" Artikel", artikel)
+artikelliste.push("Erdnuss-Sauce")
+artikel = 42
+console.log(" Artikel", artikel)
+
+const Gruppe = "😀"
+console.log(" Gemüse", Gruppe)
+
