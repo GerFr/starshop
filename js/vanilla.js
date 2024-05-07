011c96b4d7af1ae0728eb1dc912b91b5609e3a2b
GerFr <140012598+GerFr@users.noreply.github.com>
2024-05-07 13:10:41 +0200

    dynamic partials on index

--- js/vanilla.js
+++ js/vanilla.js
@@ -7,11 +7,11 @@ const $ = query => document.querySelector(query)
 const $$ = query => Array.from(document.querySelectorAll(query))
 
 /**
- *
- * @param element
- * @param event
- * @param func
- * @returns {*}
+ * Bindet einen Event-Handler (=Funktion) an ein DOM-Element
+ * @param element - das Ziel-Element, z.B. Button
+ * @param event - das Event, z.B. 'click'
+ * @param func - die aufzurufende Funktion, z.B. handleValidation
+ * @returns {*} - das Ziel-Element
  */
 const $on = (element, event, func) => {
   Array.isArray(element)
@@ -20,16 +20,52 @@ const $on = (element, event, func) => {
   return element
 }
 
-const render =  (data, template) => {
+/**
+ * Durchläuft das HTML-Dokument und rendert sämtliche Handlebars-Script-Tags
+ * @param data - die zu rendernden Daten
+ * @param template - das zu ersetzende template (tag)
+ * @returns {Promise<void>}
+ */
+const render =  async (data, template) => {
   const templates = $$(template)
   for(const source of templates) {
+    await loadPartials(source)
     const template = Handlebars.compile(source.innerHTML)
     const target = source.nextElementSibling
     target.innerHTML = template(data)
   }
 }
 
-const PIZZA_KEY = "selectedPizzas"
 
+/**
+ * Lädt Partials mit der Datei-Endung '.html' aus dem gleichen Verzeichnis
+ * @param code - der zu parsende Source-Code
+ * @returns {Promise<void>}
+ */
+async function loadPartials(code) {
+  const partialNames = code.innerText.match(/(?<={{(#>|>)).+?(?=\s)/g)
+  if (partialNames) {
+    for (let name of partialNames) {
+      name = name.trim()
+      const fileName = "partials/" + name + '.html'
+      const partialCode = await fetch(fileName).then(response => response.text())
+      Handlebars.registerPartial(name, partialCode)
+    }
+  }
+}
 
-
+//  Handlebar helpers
+  Handlebars.registerHelper({
+    eq: (v1, v2) => v1 === v2,
+    ne: (v1, v2) => v1 !== v2,
+    lt: (v1, v2) => v1 < v2,
+    gt: (v1, v2) => v1 > v2,
+    lte: (v1, v2) => v1 <= v2,
+    gte: (v1, v2) => v1 >= v2,
+    and() {
+      return Array.prototype.every.call(arguments, Boolean);
+    },
+    or() {
+      return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    }
+  });
