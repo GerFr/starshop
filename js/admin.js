d699be0c1ede95ea973e60b66938ce97718c9b5f
FritzRa <Raphaelfritz0912@gmail.com>
2024-06-10 00:40:51 +0200

    finished comments

--- js/admin.js
+++ js/admin.js
@@ -4,7 +4,6 @@
  * @returns {Array} - An array with the first element being an array of star IDs and the second being the full data array.
  */
 function getStarIdArr() {
-    // if data json item is not found in local storage, it is fetched locally and set
     if (localStorage.getItem("data") === null) {
         fetch('data/hygfull.json')
             .then(response => response.json())
@@ -14,7 +13,6 @@ function getStarIdArr() {
             })
             .catch(error => console.error('Error fetching JSON file:', error));
     }
-    // the complete star data (as a dictionary) aswell as all the StarIDs (as an array) are initiated for further use in the admin functions
     const data = JSON.parse(localStorage.getItem("data"));
     const StarIDarr = [];
     for (let i = 0; i < data.length; i++) {
@@ -29,38 +27,29 @@ function getStarIdArr() {
  * @param {number} ID - The ID of the star to be edited.
  */
 function IndexEdit(ID) {
-    // stores data and StarId array in local variables
     var temp = getStarIdArr()
     const StarIDarr = temp[0]
     const data = temp[1]
-    // adds eventlistener to the edit form used for editing an existing Star Item 
     var form = document.getElementById('editForm_' + ID);
     form.addEventListener('submit', event => {
-        // checks if form is vlaidated, if no, the form is not submitted, if yes the form gets submitted
         if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
         }
         else {
-            // gets all the required values out of the form using loop over the item key list
-            // for this to work, the key names have to be present in the individual Form IDs
             var StarIndex = StarIDarr.indexOf(ID)
             const stringList = ["HD", "ProperName", "Spectrum", "ColorIndex", "GeneralInformation", "Color"];
             for (const key in data[StarIndex]) {
-                // StarId is skipped because it is an uneditable value 
                 if (key == "StarID") {
                     continue
                 }
-                // filters out all the values that have to be saved as Strings 
                 if (stringList.includes(key)) {
                     data[StarIndex][key] = document.getElementById(key + "_" + ID).value
                 }
-                // filter for all the remaining Values that have to be added to the data as a number
                 else {
                     data[StarIndex][key] = Number(document.getElementById(key + "_" + ID).value)
                 }
             }
-            // data is saved and the location is reloaded so that the updated data can be shown
             localStorage.setItem("data", JSON.stringify(data))
             location.reload()
         }
@@ -73,14 +62,11 @@ function IndexEdit(ID) {
  * Handles the submission and validation of a create form for a new star entry.
  */
 function create() {
-    // stores data and StarId array in local variables
     var temp = getStarIdArr()
     const StarIDarr = temp[0]
     const data = temp[1]
-    // adds eventlistener to the create form used for creating a new Star Item 
     const form = document.getElementById('createForm')
     form.addEventListener('submit', event => {
-        // checks if form is vlaidated, if no, the form is not submitted, if yes the form gets submitted
         if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
@@ -88,27 +74,21 @@ function create() {
         else {
             const IdInput = document.getElementById('StarID');
             const stringList = ["HD", "ProperName", "Spectrum", "ColorIndex", "GeneralInformation", "Color"];
-            // Checks if the new ID already exists and throws an error message in that case
             if (StarIDarr.includes(Number(IdInput.value))) {
                 alert("ERROR:\nDuplicate StarId");
                 setTimeout(function () { location.reload(); }, 0)
             }
             else {
-                // gets all the required values out of the form using loop over the item key list
-                // for this to work, the key names have to be present in the individual Form IDs
                 var NewStar = {}
                 for (const key in data[0]) {
                     if (stringList.includes(key)) {
-                        // filters out all the values that have to be saved as Strings 
                         NewStar[key] = document.getElementById(key).value
                     }
                     else {
-                        // filter for all the remaining Values that have to be added to the data as a number
                         NewStar[key] = Number(document.getElementById(key).value)
                     }
                 }
                 data.push(NewStar)
-                // data is saved and the location is reloaded so that the updated data can be shown
                 localStorage.setItem("data", JSON.stringify(data))
                 location.reload()
             }
@@ -122,14 +102,11 @@ function create() {
  * @param {number} ID - The ID of the star to be deleted.
  */
 function deleteIdIndex(ID) {
-    // stores data and StarId array in local variables
     var temp = getStarIdArr()
     const StarIDarr = temp[0]
     const data = temp[1]
-    // removes Item based on its ID usin the index of the ID on StarIDarr
     const StarIndex = StarIDarr.indexOf(ID)
     data.splice(StarIndex, 1)
-    // data is saved and the location is reloaded so that the updated data can be shown
     localStorage.setItem("data", JSON.stringify(data))
     location.reload()
 }
--- js/pagination.js
+++ js/pagination.js
@@ -1,36 +1,43 @@
-
+/**
+ * jump to page 1 
+ */
 function goToStartPage() {
     const params = new URLSearchParams(window.location.search)
     params.set("page", 1)
     history.replaceState({}, "", `${window.location.pathname}?${params}`)
     location.reload()
 }
+/**
+ * jump to page given in parameter
+ * @param {*} page 
+ */
 function goToEndPage(page) {
     const params = new URLSearchParams(window.location.search)
     params.set("page", page)
     history.replaceState({}, "", `${window.location.pathname}?${params}`)
     location.reload()
 }
-
+/**
+ * logic for activating and deactivating pagination buttons
+ * @param {*} page last possible page
+ * @param {*} max last possible page
+ */
 function paginationCheck(page, max) {
     if (page <= 1) {
         document.getElementById('decrement').classList.add('d-none')
         document.getElementById('increment').classList.remove('d-none')
-        document.getElementById('centralpage').classList.add('d-none')
         document.getElementById('startPage').classList.add('d-none')
         document.getElementById('endPage').classList.remove('d-none')
     }
     if (page > 1 && page < max) {
         document.getElementById('decrement').classList.remove('d-none')
         document.getElementById('increment').classList.remove('d-none')
-        document.getElementById('centralpage').classList.remove('d-none')
         document.getElementById('startPage').classList.remove('d-none')
         document.getElementById('endPage').classList.remove('d-none')
     }
     if (page >= max && max != 1) {
         document.getElementById('decrement').classList.remove('d-none')
         document.getElementById('increment').classList.add('d-none')
-        document.getElementById('centralpage').classList.add('d-none')
         document.getElementById('startPage').classList.remove('d-none')
         document.getElementById('endPage').classList.add('d-none')
     }
@@ -39,7 +46,9 @@ function paginationCheck(page, max) {
         document.getElementById('endPage').classList.add('d-none')
     }
 }
-
+/**
+ * decrements page by one 
+ */
 function decrement() {
     const params = new URLSearchParams(window.location.search)
     var page = Number(params.get("page"))
@@ -50,7 +59,9 @@ function decrement() {
         window.location.reload()
     }
 }
-
+/**
+ * increments page by one 
+ */
 function increment() {
     const dataString = localStorage.getItem("data")
     const dataLenght = JSON.parse(dataString).length
@@ -70,7 +81,12 @@ function increment() {
         window.location.reload()
     }
 }
-
+/**
+ * returns start and end Index of items for current page and sets page in URL
+ * @param {*} page current page
+ * @param {*} pageLength amount of items on page
+ * @returns {object} [start, end, pageNum]
+ */
 function getPageNumbers(page, pageLength) {
     var start
     var end
